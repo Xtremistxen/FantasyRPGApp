f62d1968316dc62f36c3eec0b139d6e3e8a56b43
Xtremistxen <131495799+Xtremistxen@users.noreply.github.com>
2026-05-03 18:53:42 -0500

    Updated forms part 4
    added backgrounds and updated forms

--- AddCharacterForm.Designer.cs
+++ AddCharacterForm.Designer.cs
@@ -28,136 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCharacterForm));
             this.btnSaveCharacter = new System.Windows.Forms.Button();
             this.txtCharacterName = new System.Windows.Forms.TextBox();
             this.txtLevel = new System.Windows.Forms.TextBox();
             this.txtClassId = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(327, 178);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Character Name:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(327, 217);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Level:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(327, 256);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Class ID:";
             // 
             // btnSaveCharacter
             // 
-            this.btnSaveCharacter.Location = new System.Drawing.Point(339, 314);
+            this.btnSaveCharacter.BackColor = System.Drawing.Color.Transparent;
+            this.btnSaveCharacter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveCharacter.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnSaveCharacter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSaveCharacter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSaveCharacter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveCharacter.Location = new System.Drawing.Point(325, 327);
             this.btnSaveCharacter.Name = "btnSaveCharacter";
-            this.btnSaveCharacter.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveCharacter.Size = new System.Drawing.Size(132, 23);
             this.btnSaveCharacter.TabIndex = 3;
-            this.btnSaveCharacter.Text = "Save Character";
-            this.btnSaveCharacter.UseVisualStyleBackColor = true;
+            this.btnSaveCharacter.UseVisualStyleBackColor = false;
             this.btnSaveCharacter.Click += new System.EventHandler(this.btnSaveCharacter_Click);
             // 
             // txtCharacterName
             // 
-            this.txtCharacterName.Location = new System.Drawing.Point(330, 194);
+            this.txtCharacterName.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtCharacterName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCharacterName.ForeColor = System.Drawing.Color.Gold;
+            this.txtCharacterName.Location = new System.Drawing.Point(252, 143);
             this.txtCharacterName.Name = "txtCharacterName";
-            this.txtCharacterName.Size = new System.Drawing.Size(100, 20);
+            this.txtCharacterName.Size = new System.Drawing.Size(153, 20);
             this.txtCharacterName.TabIndex = 4;
             // 
             // txtLevel
             // 
-            this.txtLevel.Location = new System.Drawing.Point(330, 233);
+            this.txtLevel.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtLevel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLevel.ForeColor = System.Drawing.Color.Gold;
+            this.txtLevel.Location = new System.Drawing.Point(252, 210);
             this.txtLevel.Name = "txtLevel";
-            this.txtLevel.Size = new System.Drawing.Size(100, 20);
+            this.txtLevel.Size = new System.Drawing.Size(153, 20);
             this.txtLevel.TabIndex = 5;
             // 
             // txtClassId
             // 
-            this.txtClassId.Location = new System.Drawing.Point(330, 272);
+            this.txtClassId.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtClassId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtClassId.ForeColor = System.Drawing.Color.Gold;
+            this.txtClassId.Location = new System.Drawing.Point(252, 279);
             this.txtClassId.Name = "txtClassId";
-            this.txtClassId.Size = new System.Drawing.Size(100, 20);
+            this.txtClassId.Size = new System.Drawing.Size(153, 20);
             this.txtClassId.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(436, 201);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Enter Character name";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(436, 240);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Enter level: 1-20";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(436, 279);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(232, 13);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Enter ID: 1 for  Warrior, 2 for Mage, 3 for Rogue";
             // 
             // AddCharacterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.txtClassId);
             this.Controls.Add(this.txtLevel);
             this.Controls.Add(this.txtCharacterName);
             this.Controls.Add(this.btnSaveCharacter);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AddCharacterForm";
-            this.Text = "AddCharacterForm";
+            this.Text = "Add New Character";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnSaveCharacter;
         private System.Windows.Forms.TextBox txtCharacterName;
         private System.Windows.Forms.TextBox txtLevel;
         private System.Windows.Forms.TextBox txtClassId;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
     }
 }
--- CharactersForm.Designer.cs
+++ CharactersForm.Designer.cs
@@ -35,17 +35,17 @@
             this.charactersTableAdapter = new FatasyRPGApp.FantasyRPGDBDataSetTableAdapters.CharactersTableAdapter();
             this.tableAdapterManager = new FatasyRPGApp.FantasyRPGDBDataSetTableAdapters.TableAdapterManager();
             this.charactersBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
             this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.charactersBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.charactersDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -107,9 +107,34 @@
             this.charactersBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
             this.charactersBindingNavigator.Name = "charactersBindingNavigator";
             this.charactersBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.charactersBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.charactersBindingNavigator.Size = new System.Drawing.Size(949, 25);
             this.charactersBindingNavigator.TabIndex = 0;
             this.charactersBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
             // 
             // bindingNavigatorMoveFirstItem
             // 
@@ -138,22 +163,16 @@
             // 
             this.bindingNavigatorPositionItem.AccessibleName = "Position";
             this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
             this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
             this.bindingNavigatorPositionItem.Text = "0";
             this.bindingNavigatorPositionItem.ToolTipText = "Current position";
             // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
             // bindingNavigatorSeparator1
             // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // bindingNavigatorMoveNextItem
             // 
@@ -161,7 +180,7 @@
             this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
             this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
             this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
             this.bindingNavigatorMoveNextItem.Text = "Move next";
             // 
             // bindingNavigatorMoveLastItem
@@ -170,38 +189,20 @@
             this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
             this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
             this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
             this.bindingNavigatorMoveLastItem.Text = "Move last";
             // 
             // bindingNavigatorSeparator2
             // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
             // charactersBindingNavigatorSaveItem
             // 
             this.charactersBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.charactersBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("charactersBindingNavigatorSaveItem.Image")));
             this.charactersBindingNavigatorSaveItem.Name = "charactersBindingNavigatorSaveItem";
-            this.charactersBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.charactersBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
             this.charactersBindingNavigatorSaveItem.Text = "Save Data";
             this.charactersBindingNavigatorSaveItem.Click += new System.EventHandler(this.charactersBindingNavigatorSaveItem_Click);
             // 
@@ -215,9 +216,9 @@
             this.dataGridViewTextBoxColumn3,
             this.dataGridViewTextBoxColumn4});
             this.charactersDataGridView.DataSource = this.charactersBindingSource;
-            this.charactersDataGridView.Location = new System.Drawing.Point(12, 28);
+            this.charactersDataGridView.Location = new System.Drawing.Point(0, 28);
             this.charactersDataGridView.Name = "charactersDataGridView";
-            this.charactersDataGridView.Size = new System.Drawing.Size(776, 410);
+            this.charactersDataGridView.Size = new System.Drawing.Size(445, 421);
             this.charactersDataGridView.TabIndex = 1;
             // 
             // dataGridViewTextBoxColumn1
@@ -249,11 +250,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(949, 450);
             this.Controls.Add(this.charactersDataGridView);
             this.Controls.Add(this.charactersBindingNavigator);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CharactersForm";
-            this.Text = "CharactersForm";
+            this.Text = "Character Records";
             this.Load += new System.EventHandler(this.CharactersForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fantasyRPGDBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.charactersBindingSource)).EndInit();
