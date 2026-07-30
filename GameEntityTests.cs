1ab3dd983395df72deeb13ea61fb3a4a87cdff7d
Xtremistxen <131495799+Xtremistxen@users.noreply.github.com>
2026-07-29 19:45:34 -0500

    Add files via upload

--- GameEntityTests.cs
+++ GameEntityTests.cs
@@ -9,6 +9,7 @@ namespace FatasyRPGApp.Tests
         [Test]
         public void GameEntity_Constructor_SetsIdAndName()
         {
+            // Tests that the GameEntity constructor correctly stores the ID and name.
             GameEntity entity = new GameEntity(1, "Ancient Sword");
 
             Assert.AreEqual(1, entity.Id);
@@ -16,6 +17,7 @@ namespace FatasyRPGApp.Tests
         }
 
         [Test]
+        // Tests that GetDisplayInfo returns the expected string for a GameEntity.
         public void GameEntity_GetDisplayInfo_ReturnsCorrectInformation()
         {
             GameEntity entity = new GameEntity(2, "Magic Potion");
@@ -26,8 +28,11 @@ namespace FatasyRPGApp.Tests
         }
 
         [Test]
+
+        
         public void CharacterEntity_Constructor_SetsAllProperties()
         {
+            // Tests that the CharacterEntity constructor initializes all inherited and character specific properties.
             CharacterEntity character =
                 new CharacterEntity(3, "Aria", 10, 2);
 
@@ -40,6 +45,7 @@ namespace FatasyRPGApp.Tests
         [Test]
         public void CharacterEntity_GetDisplayInfo_ReturnsCorrectInformation()
         {
+            // Tests that CharacterEntity overrides GetDisplayInfo with character details.
             CharacterEntity character =
                 new CharacterEntity(4, "Darius", 15, 1);
 
@@ -51,6 +57,7 @@ namespace FatasyRPGApp.Tests
         [Test]
         public void QuestEntity_Constructor_SetsAllProperties()
         {
+            // Tests that the QuestEntity constructor initializes all inherited and quest specific properties.
             QuestEntity quest =
                 new QuestEntity(5, "Defeat the Goblins", 250, 3);
 
@@ -63,6 +70,7 @@ namespace FatasyRPGApp.Tests
         [Test]
         public void QuestEntity_GetDisplayInfo_ReturnsCorrectInformation()
         {
+            // Tests that QuestEntity overrides GetDisplayInfo with quest details.
             QuestEntity quest =
                 new QuestEntity(6, "Slay the Dragon", 1000, 4);
 
@@ -74,6 +82,7 @@ namespace FatasyRPGApp.Tests
         [Test]
         public void CharacterEntity_UsesPolymorphism()
         {
+            // Tests that a CharacterEntity uses the overridden GetDisplayInfo method through a GameEntity reference.
             GameEntity entity =
                 new CharacterEntity(7, "Lyra", 8, 3);
 
